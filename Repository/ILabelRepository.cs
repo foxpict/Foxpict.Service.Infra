d043174b103b13e49f902760d98842ae8dd4f769
atachimiko <atachi.miko@gmail.com>
2018-10-13 19:33:17 +0900

    ラベルの正規化に対応

--- Repository/ILabelRepository.cs
+++ Repository/ILabelRepository.cs
@@ -2,41 +2,50 @@ using System.Collections.Generic;
 using System.Linq;
 using Foxpict.Service.Infra.Model;
 
-namespace Foxpict.Service.Infra.Repository
-{
-    public interface ILabelRepository : IRepositoryBase
-    {
-        ILabel New();
+namespace Foxpict.Service.Infra.Repository {
+  public interface ILabelRepository : IRepositoryBase {
+    ILabel New ();
 
-        ILabel Load(long id);
+    ILabel Load (long id);
 
-        ILabel LoadByName(string name);
+    /// <summary>
+    /// ラベル名に一致するラベル情報を取得する
+    /// </summary>
+    /// <param name="name">正規化ラベル名</param>
+    /// <returns></returns>
+    ILabel LoadByName (string name);
 
-        /// <summary>
-        /// ラベル名に一致するラベル情報を取得する
-        /// </summary>
-        /// <param name="name">ラベル名</param>
-        /// <param name="ownerType">一致するオーナータイプの検索文字列</param>
-        /// <returns></returns>
-        ILabel LoadByName(string name, string ownerType);
+    /// <summary>
+    /// ラベル名に一致するラベル情報を取得する
+    /// </summary>
+    /// <param name="name">正規化ラベル名</param>
+    /// <param name="ownerType">一致するオーナータイプの検索文字列</param>
+    /// <returns></returns>
+    ILabel LoadByName (string name, string ownerType);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <param name="parentLabelId"></param>
-        /// <returns></returns>
-        IQueryable<ILabel> FindChildren(long parentLabelId);
+    /// <summary>
+    ///
+    /// </summary>
+    /// <param name="parentLabelId"></param>
+    /// <returns></returns>
+    IQueryable<ILabel> FindChildren (long parentLabelId);
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <returns></returns>
-        IQueryable<ILabel> FindRoot();
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    IQueryable<ILabel> FindRoot ();
 
-        /// <summary>
-        ///
-        /// </summary>
-        /// <returns></returns>
-        IEnumerable<ILabel> GetAll();
-    }
+    /// <summary>
+    ///
+    /// </summary>
+    /// <returns></returns>
+    IEnumerable<ILabel> GetAll ();
+
+    /// <summary>
+    /// 正規化ラベル名を更新する
+    /// </summary>
+    /// <param name="label"></param>
+    void UpdateNormalizeName (ILabel label);
+  }
 }
