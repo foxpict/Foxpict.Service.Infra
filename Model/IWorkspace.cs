094eb202b30205e7f63a308cd5e509f77c1189d6
atachimiko <atachi.miko@gmail.com>
2018-10-21 00:53:34 +0900

    インポート領域への対応

--- Model/IWorkspace.cs
+++ Model/IWorkspace.cs
@@ -1,12 +1,33 @@
-namespace Foxpict.Service.Infra.Model
-{
-    public interface IWorkspace : Foxpict.Common.Model.IWorkspace
-    {
-        /// <summary>
-        /// 指定した文字列からワークスペースのパス部分を削除した文字列を返します。
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        string TrimWorekspacePath(string path);
-    }
+using System.IO;
+
+namespace Foxpict.Service.Infra.Model {
+  public interface IWorkspace : Foxpict.Common.Model.IWorkspace {
+    /// <summary>
+    /// 指定した文字列からワークスペースのパス部分を削除した文字列を返します。
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
+    string TrimWorekspacePath (string path);
+
+    /// <summary>
+    /// インポート領域内のファイルかどうかを検証します。
+    /// </summary>
+    /// <param name="fileSystemInfo"></param>
+    /// <returns></returns>
+    bool ContainsImport (FileSystemInfo fileSystemInfo);
+
+    /// <summary>
+    /// 仮想領域内のファイルかどうかを検証します。
+    /// </summary>
+    /// <param name="fileSystemInfo"></param>
+    /// <returns></returns>
+    bool ContainsVirtual (FileSystemInfo fileSystemInfo);
+
+    /// <summary>
+    /// 物理領域内のファイルかどうかを検証します。
+    /// </summary>
+    /// <param name="fileSystemInfo"></param>
+    /// <returns></returns>
+    bool ContainsPhysical (FileSystemInfo fileSystemInfo);
+  }
 }
